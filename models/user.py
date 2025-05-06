48f997d92b93341494a3b9e64fa5354de275e729
Kodien <aldanalif25@gmail.com>
2025-05-07 05:29:13 +0700

    Fixed some errors in the database

--- models/user.py
+++ models/user.py
@@ -10,5 +10,5 @@ class UserModel(db.Model): # Defines the user model and allows us to manage it
     # Creates a column in the database named 'id' which is an int and is set as a unique identifier (primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False) 
     #Creates the username column which is a string with a max length of 80. Should be unique and can't be null
-    password = db.Column(db.String(80), nullable=False) 
+    password = db.Column(db.String(256), nullable=False) 
     # Creates the password column which is a string with a max length of 80 and can't be null
